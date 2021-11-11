c9afd01328e7ef2c89d50c70dd46ea69b60268d7
Roger <luo.jie024@gmail.com>
2021-11-11 17:32:31 +0800

    vpn证书创建脚本交互方法写好了

--- vpn/vpn/test.py
+++
@@ -1,6 +0,0 @@
-import tkinter.messagebox
-import tkinter
-root = tkinter.Tk()
-root.withdraw() #****实现主窗口隐藏
-root.update()
-tkinter.messagebox.showinfo('提示',' 实现主窗口隐藏withdraw!')
--- vpn/vpn/test2.py
+++ vpn/vpn/test2.py
@@ -5,19 +5,25 @@ ssh_port = 22
 ssh_username = "root"
 ssh_password = "Roger645174748"
 
-def insertuser(u,p):
+def createcrt(u):
     ssh_client = paramiko.SSHClient()
 
     '''解决目标服务器known_hosts不存在请求者信息的报错'''
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     '''建立ssh连接'''
-
-    #ssh_client.connect(hostname="192.168.149.100", port=22, username="root", password="Roger645174748")
     ssh_client.connect(hostname=ssh_ip, port=ssh_port, username=ssh_username, password=ssh_password)
-    '''拼接shell语句'''
-    insertusercmd = "echo " + u + " " + p + ">> /tmp/test"
-    ssh_client.exec_command(insertusercmd)
 
+    '''执行VPN开通脚本'''
+    stdin, stdout, stder = ssh_client.exec_command("sh /data/openvpn-install-master/openvpn-install.sh")
+    '''输入1，选择新建用户'''
+    stdin.write('1\n')
+    '''输入新用户用户名'''
+    stdin.write(u)
+    '''输入回车'''
+    stdin.write('\n')
+    stdin.flush()
+    print(stdout.read())
 if __name__ == '__main__':
 
-    insertuser(u="luojie23", p="luojie22")
+    createcrt(u="jintian")
+
